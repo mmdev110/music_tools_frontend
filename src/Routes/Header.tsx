5a1660655658b793187675a2ee4a3c0fe7a9cef6
mmdev110 <mmdev8787@gmail.com>
2022-12-07 18:43:49 +0900

    add tailwindcss

--- src/Routes/Header.tsx
+++ src/Routes/Header.tsx
@@ -5,34 +5,36 @@ import { TERMS } from 'Constants'
 import Detail from 'Routes/Detail'
 import * as Types from 'types'
 import * as Utils from 'utils'
-import 'App.css'
 import { forEachChild } from 'typescript'
 type props = {
     user: Types.User | null
 }
 const Header = ({ user }: props) => {
     return (
-        <div className="App">
+        <div className="bg-stone-600 font-mono text-slate-100">
             <div>Header Page</div>
             {user ? (
                 <div>{`Welcome! ${user.email}`}</div>
             ) : (
                 'You are not logged in.'
             )}
-            {user ? (
-                <Link to="signout">SignOut</Link>
-            ) : (
-                <div>
-                    <div>
-                        <Link to="signin">SignIn</Link>
+            <div className="flex justify-center">
+                {user ? (
+                    <Link to="signout">SignOut</Link>
+                ) : (
+                    <div className="flex">
+                        <div>
+                            <Link to="signin">SignIn</Link>
+                        </div>
+                        <div>
+                            <Link to="signup">SignUp</Link>
+                        </div>
                     </div>
-                    <div>
-                        <Link to="signup">SignUp</Link>
-                    </div>
-                </div>
-            )}
-            <Link to="list">List</Link>
-            <Link to="/">Top</Link>
+                )}
+                <Link to="list">List</Link>
+                <Link to="/">Top</Link>
+            </div>
+
             <Outlet />
         </div>
     )
