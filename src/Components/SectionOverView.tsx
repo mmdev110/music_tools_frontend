ef1ff3a594a2eecce948bd57f808bd86413a61cd
mmdev110 <mmdev8787@gmail.com>
2023-07-30 09:44:47 +0900

    refactoring

--- src/Components/SectionOverView.tsx
+++ src/Components/SectionOverView.tsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect, useRef, SyntheticEvent } from 'react'
 import Dropzone from 'react-dropzone'
 import HLS from 'hls.js'
-import { AudioRange, UserSongSection, UserSongInstrument } from 'types/front'
+import {
+    AudioRange,
+    UserSongSection,
+    UserSongInstrument,
+    User,
+} from 'types/front'
 import { INSTRUMENT_CATEGORIES } from 'config/front'
 import { Button, Input } from 'Components/HTMLElementsWrapper'
 import OneTag from 'Components/OneTag2'
@@ -32,131 +37,80 @@ const SectionOverView = ({
 
     const column: (UserSongSection | string)[] = ['instname', ...sections] //縦軸
 
-    const renderColumn = (
-        valueCol: UserSongSection | string,
-        indexCol: number
-    ) => {
-        //kusosugi
+    //左端の一列目
+    const renderInstNamesCol = (indexCol: number) => {
+        let style = 'text-sm border-2 basis-12 border-black'
         return (
             <div className="flex basis-20 flex-col" key={indexCol}>
-                {row.map((valueRow, indexRow) => {
+                {row.map((rowValue, rowIndex) => {
                     let value: React.ReactNode = ''
-                    let style = 'text-sm border-2 basis-12 border-black'
-                    let onCellClick = () => {}
-                    if (typeof valueRow !== 'string') {
-                        const inst = valueRow as UserSongInstrument
-                        if (valueCol === 'instname') {
-                            //左端
+                    switch (rowValue) {
+                        case 'sectionname':
+                            value = 'section'
+                            break
+                        case 'bars':
+                            value = 'bars'
+                            break
+                        case 'play':
+                            value = 'play'
+                            break
+                        case 'copy':
+                            value = 'copy'
+                            break
+                        default:
+                            const inst = rowValue as UserSongInstrument
                             value = (
                                 <OneTag
-                                    color={'bg-gray-400'}
-                                    onClick={() => {}}
-                                    onRightClick={() => {}}
+                                    color="bg-gray-400"
                                     name={inst.name}
                                     tooltipText={inst.memo}
                                 />
                             )
-                        } else {
-                            //const indexInst = indexRow - 2
-                            const indexSec = indexCol - 1
-                            const foundIndex = sections[
-                                indexSec
-                            ].instruments.findIndex(
-                                (instSec) => instSec.name === inst.name
-                            )
-                            let bgcolor = 'bg-transparent'
-                            if (foundIndex !== -1) {
-                                //存在する場合、背景色を変える
-                                if (inst.category === INSTRUMENT_CATEGORIES[0])
-                                    bgcolor = 'bg-sky-500'
-                                if (inst.category === INSTRUMENT_CATEGORIES[1])
-                                    bgcolor = 'bg-red-500'
-                                if (inst.category === INSTRUMENT_CATEGORIES[2])
-                                    bgcolor = 'bg-green-500'
-                            }
-
-                            style += ` ${bgcolor} `
-                            onCellClick = () => {
-                                const newSections = [...sections]
-                                if (foundIndex !== -1) {
-                                    //sectionsにある場合、foundIndexを削除する
-                                    newSections[indexSec].instruments.splice(
-                                        foundIndex,
-                                        1
-                                    )
-                                } else {
-                                    //sectionsにない場合、instrumentを追加する
-                                    newSections[indexSec].instruments.push(inst)
-                                }
-                                onClick(newSections)
-                            }
-                        }
-                    } else {
-                        if (valueCol !== 'instname') {
-                            const section = valueCol as UserSongSection
-                            //instがない=上部の2列分の表示
-                            if (valueRow === 'sectionname') {
-                                //sectionName
-                                value = section.section
-                            } else if (valueRow === 'play') {
-                                //再生ボタン
-                                value = section.audioRanges[0] ? (
-                                    <Button
-                                        onClick={() => {
-                                            onClickPlayButton(
-                                                section.audioRanges[0]
-                                            )
-                                        }}
-                                    >
-                                        ▷
-                                    </Button>
-                                ) : (
-                                    ''
-                                )
-                            } else if (valueRow === 'copy') {
-                                //コピーボタン
-                                value = section.audioRanges[0] ? (
-                                    <Button
-                                        onClick={() => {
-                                            const indexSec = indexCol - 1
-                                            if (indexSec > 0) {
-                                                const newSections = [
-                                                    ...sections,
-                                                ]
-                                                newSections[
-                                                    indexSec
-                                                ].instruments = structuredClone(
-                                                    newSections[indexSec - 1]
-                                                        .instruments
-                                                )
-                                                onClick(newSections)
-                                            }
-                                        }}
-                                    >
-                                        copy
-                                    </Button>
-                                ) : (
-                                    ''
-                                )
-                            } else if (valueRow === 'bars') {
-                                value = section.barLength
-                            }
-                        } else {
-                            value =
-                                valueRow === 'bars'
-                                    ? 'bars'
-                                    : valueRow === 'sectionname'
-                                    ? ''
-                                    : ''
-                        }
                     }
-
+                    return <div className={style}>{value}</div>
+                })}
+            </div>
+        )
+    }
+    //2列目以降
+    const renderInstCol = (sectionIndex: number, indexCol: number) => {
+        const section = sections[sectionIndex]
+        return (
+            <div className="flex basis-20 flex-col" key={indexCol}>
+                {row.map((rowValue, rowIndex) => {
+                    let style = 'text-sm border-2 basis-12 border-black'
+                    let value: React.ReactNode = ''
+                    let onClick = () => {}
+                    switch (rowValue) {
+                        case 'sectionname':
+                            value = section.section
+                            break
+                        case 'bars':
+                            value = section.barLength
+                            break
+                        case 'play':
+                            value =
+                                section.audioRanges[0] && playButton(section)
+                            break
+                        case 'copy':
+                            value = '-'
+                            break
+                        default:
+                            const inst = rowValue as UserSongInstrument
+                            //sectionにinstが含まれてるかどうか
+                            const foundIndex = section.instruments.findIndex(
+                                (i) => i.name === inst.name
+                            )
+                            style += ` ${instCellColor(
+                                inst,
+                                foundIndex !== -1
+                            )}`
+                            value = ''
+                            onClick = () =>
+                                onInstCellClick(sectionIndex, foundIndex, inst)
+                    }
                     return (
-                        <div
-                            key={indexRow}
-                            className={style}
-                            onClick={onCellClick}
-                        >
+                        <div className={style} onClick={onClick}>
                             {value}
                         </div>
                     )
@@ -164,9 +118,71 @@ const SectionOverView = ({
             </div>
         )
     }
+    const playButton = (section: UserSongSection) => {
+        return (
+            <Button
+                onClick={() => {
+                    onClickPlayButton(section.audioRanges[0])
+                }}
+            >
+                ▷
+            </Button>
+        )
+    }
+    const copyButton = (sectionIndex: number) => {
+        return (
+            <Button
+                onClick={() => {
+                    if (sectionIndex > 0) {
+                        const newSections = [...sections]
+                        newSections[sectionIndex].instruments = structuredClone(
+                            newSections[sectionIndex - 1].instruments
+                        )
+                        onClick(newSections)
+                    }
+                }}
+            >
+                copy
+            </Button>
+        )
+    }
+
+    const renderColumn2 = (
+        valueCol: UserSongSection | string,
+        indexCol: number
+    ) => {
+        if (valueCol === 'instname') {
+            return renderInstNamesCol(0)
+        } else {
+            const sectionIndex = indexCol - 1
+            return renderInstCol(sectionIndex, indexCol)
+        }
+    }
+    const instCellColor = (inst: UserSongInstrument, isFound: boolean) => {
+        if (!isFound) return 'bg-transparent'
+        if (inst.category === INSTRUMENT_CATEGORIES[0]) return 'bg-sky-500'
+        if (inst.category === INSTRUMENT_CATEGORIES[1]) return 'bg-red-500'
+        if (inst.category === INSTRUMENT_CATEGORIES[2]) return 'bg-green-500'
+        return 'bg-transparent'
+    }
+    const onInstCellClick = (
+        sectionIndex: number,
+        foundIndex: number,
+        inst: UserSongInstrument
+    ) => {
+        const newSections = [...sections]
+        if (foundIndex !== -1) {
+            //sectionsにある場合、section.instrumentsからfoundIndexを削除する
+            newSections[sectionIndex].instruments.splice(foundIndex, 1)
+        } else {
+            //sectionsにない場合、instrumentを追加する
+            newSections[sectionIndex].instruments.push(inst)
+        }
+        return onClick(newSections)
+    }
     return (
         <div className="flex">
-            {column.map((value, index) => renderColumn(value, index))}
+            {column.map((value, index) => renderColumn2(value, index))}
         </div>
     )
 }
